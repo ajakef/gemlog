e83aa87ea374f5b1d183924e5e280c54a821c95c
Kevin Anderson <kevinanderso@gmail.com>
2020-09-02 19:53:07 -0600

    move test to the new test location

--- gemlog/test_unit_ReadGem_v0_9_single.py
+++ gemlog/test_unit_ReadGem_v0_9_single.py
@@ -19,6 +19,23 @@ def test_ReadGem_v0_9_single():
             assert np.abs(L0[i][x] - L1[i][x]).max() < eps
             assert L0[i][x].dtype == L1[i][x].dtype
 
+
+def test_read_with_cython():
+    ## Check to make sure that the function does not crash when given good input, and that the
+    ## output types and values are exactly correct. Flaws in the output (including hard-to-see
+    ## flaws) cause hard-to-trace problems downstream.
+    fn = 'data/FILE0001.077' 
+    offset = 72000000.0 + 5263 # this is a reasonable offset to use for this file; can't use 0 because bugs might slip through
+    eps = 1e-5
+    L1 = read_with_cython(fn, offset)
+    L0 = slow_ReadGem_v0_9_single(fn, offset)
+    assert np.abs(L1['data'] - L0['data']).max() < eps
+    for i in ['gps', 'metadata']:
+        for x in L0[i].keys():
+            assert np.abs(L0[i][x] - L1[i][x]).max() < eps
+            assert L0[i][x].dtype == L1[i][x].dtype
+
+
 ## These tests ensure that it raises an exception when reading bad raw
 ## files. No need to classify the type of exception; any exception
 ## should be interpreted as a bad file.
