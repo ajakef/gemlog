426bde471819d45cf072e18b7ffa905e752d40bf
Jake Anderson <ajakef@gmail.com>
2020-09-04 19:02:09 -0600

    new version 0.3.1 for exception logging improvements

--- gemlog/version.py
+++ gemlog/version.py
@@ -1,1 +1,8 @@
-__version__  = '0.3.0'
+## List of old versions is not yet comprehensive
+
+__version__  = '0.3.1' #  more helpful logging output
+#__version__  = '0.3.0' # 2020-09-04, cython added
+#__version__  = '0.2.3' # improving testing, including empty/bad files
+#__version__  = '0.2.2' # automated github tests, setup.py improvements, and modelst speed-up
+#__version__  = '0.2.1' # 
+#__version__ = '0.0.5' # added new functions to make network map from gps data and rename mseeds from serial_number.channel to network.station.location.channel codes
