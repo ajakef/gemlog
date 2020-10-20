2c589fe1e777e405ca760ceeb5f9548c16fa79e0
Jake Anderson <ajakef@gmail.com>
2020-10-20 16:34:37 -0600

    added huddle test code

---
+++ gemlog/huddle_test.py
@@ -0,0 +1,28 @@
+## Huddle test performance requirements:
+#### >=3 loggers must have barbs facing each other and all within 15 cm, in a turbulence-suppressed semi-enclosed space, sitting on a shared hard surface on top of padding, with good GPS signal, in a site that is not next to a continuous noise source (duty cycle < 80%). Loggers should all start and stop acquisition within 1 minute of each other, and run for at least one week.
+
+## Metadata:
+#### battery voltage must be in reasonable range (1.7 to 15 V)
+#### temperature must be in reasonable range (-20 to 60 C)
+#### at every given time, temperature must agree within 2C for all loggers
+#### A2 and A3 must be 0-3.1, and dV/dt = 0 should be true <1% of record
+#### minFifoFree and maxFifoUsed should always add to 75
+#### maxFifoUsed should be less than 5 99% of the time, and should never exceed 25
+#### maxOverruns should always be zero
+#### unusedStack1 and unusedStackIdle should always be >50 (this could change)
+#### gpsOnFlag should never be on for more than 3 minutes at a time
+#### all loggers' first and last times should agree within 20 minutes
+
+## GPS:
+#### lat and lon should vary by less than 100 m for each logger (could change)
+#### all loggers' average lat and lon should agree within 1 m
+#### the maximum difference between GPS fix times should never exceed 20 minutes
+#### all loggers' first and last GPS times should agree within 20 minutes
+
+## Waveform data (excluding first and last hour)--all of these could change:
+#### length of converted data should match among all loggers
+#### a "coherent window" has all cross-correlation coefficients > 0.9, passes consistency criterion, and has amplitude above noise spec. 90% of coherent windows should have only nonzero lags, and none should have persistently nonzero lags (define).
+#### dp/dt = 0 should occur for <1% of record (e.g. clipping, flatlining)
+#### noise spectrum must exceed spec/2
+#### noise spectra of sensors must agree within 3 dB everywhere and within 1 dB for 90% of frequencies
+#### 20% quantile spectra should be close to self-noise spec (check to see if this holds up)
