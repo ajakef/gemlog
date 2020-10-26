8de6f988bb232e3326c2f98c42d628135eaaf9fd
Jake Anderson <ajakef@gmail.com>
2020-10-26 16:34:37 -0600

    'debugging deployment code'

--- gemlog/version.py
+++ gemlog/version.py
@@ -1,4 +1,5 @@
-__version__ = '1.2.1' # Run on mix of good raw files, no-gps raw files, and empty files without errors 
+__version__ = '1.2.2' # debugging deployment code 
+#__version__ = '1.2.1' # Run on mix of good raw files, no-gps raw files, and empty files without errors 
 #__version__ = '1.2.0' # Handle missing GPS data more gracefully 
 #__version__ = '1.1.2' # added output file length option to gemconvert, and fixed some missing_gps issues
 #__version__ = '1.1.1' # moved drift correction from block-level to file-level
@@ -13,4 +14,4 @@ __version__ = '1.2.1' # Run on mix of good raw files, no-gps raw files, and empt
 #__version__  = '0.2.2' # automated github tests, setup.py improvements, and modelst speed-up
 #__version__  = '0.2.1' # 
 #__version__ = '0.0.5' # added new functions to make network map from gps data and rename mseeds from serial_number.channel to network.station.location.channel codes
-## List of old versions is not yet comprehensive###
+## List of old versions is not yet comprehensive####
