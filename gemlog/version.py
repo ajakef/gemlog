a9714d9b77b2c443945c45a4be722b67f8e3dce6
Jake Anderson <ajakef@gmail.com>
2020-10-26 14:20:13 -0600

    'Handle missing GPS data more gracefully'

--- gemlog/version.py
+++ gemlog/version.py
@@ -1,4 +1,5 @@
-__version__ = '1.1.2' # added output file length option to gemconvert, and fixed some missing_gps issues
+__version__ = '1.2.0' # Handle missing GPS data more gracefully 
+#__version__ = '1.1.2' # added output file length option to gemconvert, and fixed some missing_gps issues
 #__version__ = '1.1.1' # moved drift correction from block-level to file-level
 #__version__ = '1.1.0' # added new gem_cat feature for combining no-GPS files
 #__version__ = '1.0.2' # changed make_gem_inventory so that output stationXML passes the stationXML validator: https://github.com/iris-edu/StationXML-Validator/
@@ -11,4 +12,4 @@ __version__ = '1.1.2' # added output file length option to gemconvert, and fixed
 #__version__  = '0.2.2' # automated github tests, setup.py improvements, and modelst speed-up
 #__version__  = '0.2.1' # 
 #__version__ = '0.0.5' # added new functions to make network map from gps data and rename mseeds from serial_number.channel to network.station.location.channel codes
-## List of old versions is not yet comprehensive
+## List of old versions is not yet comprehensive##
