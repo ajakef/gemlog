0f1e7182b8e93427141dd2dba12fb7ea95381132
Jake Anderson <ajakef@gmail.com>
2020-11-06 12:42:12 -0700

    more bug fixes

--- gemlog/version.py
+++ gemlog/version.py
@@ -1,4 +1,5 @@
-__version__ = '1.2.6' # bug fixes to improve support for raw formats 0.8 and 0.85 and to make converting large datasets more reliable 
+__version__ = '1.2.7' # more bug fixes 
+#__version__ = '1.2.6' # bug fixes to improve support for raw formats 0.8 and 0.85 and to make converting large datasets more reliable 
 #__version__ = '1.2.5' # added support for raw format version 0.8 
 #__version__ = '1.2.4' # bug fix in convert 
 #__version__ = '1.2.3' # Changes to installation procedure (PyPI now available) and minor improvements to gemconvert 
@@ -18,4 +19,4 @@ __version__ = '1.2.6' # bug fixes to improve support for raw formats 0.8 and 0.8
 #__version__  = '0.2.2' # automated github tests, setup.py improvements, and modelst speed-up
 #__version__  = '0.2.1' # 
 #__version__ = '0.0.5' # added new functions to make network map from gps data and rename mseeds from serial_number.channel to network.station.location.channel codes
-## List of old versions is not yet comprehensive########
+## List of old versions is not yet comprehensive#########
