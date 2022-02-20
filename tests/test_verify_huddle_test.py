eb702a878729866b84cf8de3e9b4a8b14c9bc5eb
Jake Anderson <ajakef@gmail.com>
2022-02-20 01:01:36 -0700

    minor changes to tests and __init__.py

--- tests/test_verify_huddle_test.py
+++ tests/test_verify_huddle_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest, shutil, os, obspy
 import gemlog
+from gemlog.huddle_test import verify_huddle_test
 
 def setup_module():
     try:
@@ -16,7 +17,7 @@ def teardown_module():
 
 
 def test_verify_huddle_test():
-    gemlog.huddle_test.verify_huddle_test('../data/test_data/huddle_test')
+    verify_huddle_test('../data/test_data/huddle_test')
     # this test has disagreeing stop times because a battery died. add a check for that.
     # report is made in data folder. need to add an option to make the report in any folder.
     
