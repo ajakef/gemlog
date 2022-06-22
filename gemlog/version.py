443fe95f3c72dd15730bf3121662ab0fec89b8fd
Jake Anderson <ajakef@gmail.com>
2022-06-22 11:45:34 -0600

    upgrades dependencies due to moderate security issue in numpy: https://github.com/advisories/GHSA-fpfv-jqm9-f5jm

--- gemlog/version.py
+++ gemlog/version.py
@@ -1,4 +1,5 @@
-__version__ = '1.6.3' # minor improvements/bug fixes for verify_huddle_test 
+__version__ = '1.6.4' # upgrades dependencies due to moderate security issue in numpy: https://github.com/advisories/GHSA-fpfv-jqm9-f5jm 
+#__version__ = '1.6.3' # minor improvements/bug fixes for verify_huddle_test 
 #__version__ = '1.6.2' # bug fix 
 #__version__ = '1.6.1' # added new tools to convert data with no gps or inadequate gps (at user's own risk!) 
 #__version__ = '1.6.0' # now supports raw format 1.10 (compact) 
@@ -55,4 +56,4 @@ __version__ = '1.6.3' # minor improvements/bug fixes for verify_huddle_test
 #__version__  = '0.2.2' # automated github tests, setup.py improvements, and modelst speed-up
 #__version__  = '0.2.1' # 
 #__version__ = '0.0.5' # added new functions to make network map from gps data and rename mseeds from serial_number.channel to network.station.location.channel codes
-## List of old versions is not yet comprehensive###############################################
+## List of old versions is not yet comprehensive################################################
