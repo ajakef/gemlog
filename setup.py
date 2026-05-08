c37d8513cd3c93faddcd4b2b1a2a7dca0e9b20b4
Jake Anderson <ajakef@gmail.com>
2026-05-08 14:07:40 -0400

    updated requirements due to breaking changes in numpy and pandas

--- setup.py
+++ setup.py
@@ -62,6 +62,8 @@ INSTALL_REQUIRES = [
     'obspy>=1.3',
     'numpy>=1.22', 
     'pandas>=1.3.3', # minimum for numpy 1.22 compatibility
+    'numpy>=1.22,<2.0', # breaking changes in 2.*
+    'pandas>=1.3.3,<3.0', # minimum for numpy 1.22 compatibility; <3.0 for API compatibility
     'scipy>=1.10.0', # 1.10 for a security update in July 2023
     'matplotlib>=3.7.0', # required for compatibility with pandas>2
     'cython', # used for reading raw files quickly
